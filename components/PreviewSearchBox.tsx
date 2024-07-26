2828e7befd67ade357fd6571af56133925f2eb1b
Gloria Hornero <snootchie.boochies@gmail.com>
2024-07-26 11:35:23 +0200

    investigating

--- components/PreviewSearchBox.tsx
+++ components/PreviewSearchBox.tsx
@@ -3,75 +3,149 @@
 import { actionSearchBooks, Book } from '@/actions/search-books';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
-import { useEffect, useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 
+interface Option {
+  label: string;
+  imageUrl?: string;
+  index: number;
+}
 
 export default function PreviewSearchBox() {
+  const [options, setOptions] = useState<Option[]>([]);
+  //const [noOptionsText, setNoOptionsText] = useState("Waiting for your input!");
+ //const [search, setSearch] = useState('');
 
-    const [options, setOptions] = useState<{ label: string, imageUrl: string | undefined, index: number }[]>([]);
-    const [noOptionsSearch, setNoOptionsSearch] = useState("Waiting for your input!")
+  const handleInputChange = useCallback(
+    async (event: React.SyntheticEvent, query: string) => {
+      const inputEvent = event.nativeEvent as InputEvent;
+      if (
+        inputEvent.inputType === 'deleteContentBackward' || 
+        event.nativeEvent instanceof FocusEvent
+      ) {
+        setOptions([]);
+       // setNoOptionsText("Waiting for your input");
+      } else {
+       // setNoOptionsText("Searching...");
 
-    
-    return (
-        <Autocomplete
-            className="bg-white"
-            noOptionsText={noOptionsSearch}
-            size="small"
-            disablePortal
-            forcePopupIcon={false}
-            onInputChange={ (event, query) => {
+        try {
+          const result = await actionSearchBooks(query, 0, 5);
 
-                const input = event.nativeEvent as InputEvent
-                input.inputType
-                console.log(input)
-                if (input.inputType === 'deleteContentBackward' || event.nativeEvent instanceof FocusEvent) {
-                    setOptions([]);
-                    setNoOptionsSearch("Waiting for your input")
-                } else {
-                    setNoOptionsSearch("Searching....")
-                    actionSearchBooks(query, 0, 5).then(result => {
+          console.log(result.books)
+          
+          const mappedOptions = result.books.map((book, index) => ({
+            label: `${book.title} by ${book.authors}`,
+            imageUrl: book.smallThumbnail,
+            index,
+          }));
+
+          mappedOptions.push( {
+            label: "See all results",
+            imageUrl: "test",
+            index:0
+          });
+
+          console.log('Mapped options', mappedOptions)
+
+          //setOptions(mappedOptions.length > 0 ? mappedOptions : []);
+          setOptions(mappedOptions)
+          //setSearch(query);
+        } catch (error) {
+          console.error("Error fetching books:", error);
+          //setNoOptionsText("Error fetching results. Please try again.");
+        }
+      }
+    },
+    [] 
+  );
+
+  const handleSomething = useCallback ((props, option) => {
     
-                        
-                        const mappedOptions = result.books.map((result, index) => ({ label: `${result.title} by ${result.authors}`, imageUrl: result.smallThumbnail, index: index }));
-                        
-                        console.log('LENGTH', mappedOptions.length)
+    //console.log('Props', props);
+    console.log('Option', option)
     
-                        if(mappedOptions.length === 5) {
-                            mappedOptions.push({
-                                label: 'LOLOLOLO',
-                                imageUrl: undefined,
-                                index: 0
-                            })
-    
-                            console.log('les opsions', mappedOptions)
-                        }
-                        if (mappedOptions.length > 0) {
-                             setOptions(mappedOptions);
-                        }
-                    });
-                }
-            }}
-            options={options}
-            renderOption={(props, option) => (
-                <li {...props}>
-                    {option.imageUrl && <img src={option.imageUrl} alt={option.label} style={{ width: 50, height: 75, marginRight: 10 }} />}
-                    {option.label}
-                </li>
-                )}
-            sx={{ width: 500 }}
-            renderInput={(params) => <TextField {...params} sx={{
-                '& .MuiOutlinedInput-root': {
-                    '& fieldset': {
-                        borderColor: 'white', // Cambia el color del borde aquí
-                    },
-                    '&:hover fieldset': {
-                        borderColor: 'white', // Color del borde al pasar el ratón
-                    },
-                    '&.Mui-focused fieldset': {
-                        borderColor: 'white', // Color del borde cuando está enfocado
-                    },
-                },
-            }} label="Search..." />}
+    return  (<li {...props}>
+    {option.imageUrl && (
+      <img
+        src={option.imageUrl}
+        alt={option.label}
+        style={{ width: 50, height: 75, marginRight: 10 }}
+      />
+    )}
+    {option.label} 
+  </li>)
+  }, [])
+  /*
+  (props, option) => (
+    <li {...props}>
+      {option.imageUrl && (
+        <img
+          src={option.imageUrl}
+          alt={option.label}
+          style={{ width: 50, height: 75, marginRight: 10 }}
         />
-    );
+      )}
+      {option.label}
+    </li>
+  )*/
+
+  /*
+  useEffect(() => {
+    setOptions(
+      [
+        ...options,
+        {
+          label: "See all results",
+          imageUrl: undefined,
+          index:0
+        }
+      ])
+  }, [search]); */
+
+  /*
+  renderOption={(props, option) => (
+    <li {...props}>
+      {option.imageUrl && (
+        <img
+          src={option.imageUrl}
+          alt={option.label}
+          style={{ width: 50, height: 75, marginRight: 10 }}
+        />
+      )}
+      {option.label}
+    </li>
+  )}*/
+
+  return (
+    <Autocomplete
+      className="bg-white"
+      //noOptionsText={noOptionsText}
+      size="small"
+      disablePortal
+      forcePopupIcon={false}
+      onInputChange={handleInputChange}
+      options={options}
+      renderOption={handleSomething}
+      sx={{ width: 500 }}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          sx={{
+            '& .MuiOutlinedInput-root': {
+              '& fieldset': {
+                borderColor: 'white', // Cambia el color del borde aquí
+              },
+              '&:hover fieldset': {
+                borderColor: 'white', // Color del borde al pasar el ratón
+              },
+              '&.Mui-focused fieldset': {
+                borderColor: 'white', // Color del borde cuando está enfocado
+              },
+            },
+          }}
+          label="Search..."
+        />
+      )}
+    />
+  );
 }
