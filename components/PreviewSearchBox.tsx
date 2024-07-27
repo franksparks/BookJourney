3937605397711566aa25865e26795f94868bb960
Gloria Hornero <snootchie.boochies@gmail.com>
2024-07-27 19:17:17 +0200

    adds debounced search

--- components/PreviewSearchBox.tsx
+++ components/PreviewSearchBox.tsx
@@ -4,6 +4,7 @@ import { actionSearchBooks } from "@/actions/search-books";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
 import { useState, useCallback, HTMLAttributes } from "react";
+import debounce from "lodash/debounce";
 
 interface Option {
   label: string;
@@ -12,35 +13,45 @@ interface Option {
 }
 
 export default function PreviewSearchBox() {
+
   const [options, setOptions] = useState<Option[]>([]);
   const [inputValue, setInputValue] = useState("");
 
-  const handleInputChange = useCallback(
-    async (_event: React.SyntheticEvent, query: string) => {
+  const searchBooks = async (query: string) => {
+    try {
       setInputValue(query);
-      try {
-        if (query) {
-          const result = await actionSearchBooks(query, 0, 5);
-          const mappedOptions = result.books.map((book, index) => ({
-            label: `${book.title} by ${book.authors}`,
-            imageUrl: book.smallThumbnail,
-            index,
-          }));
-          mappedOptions.push({
-            label: "See all results",
-            imageUrl: "",
-            index: 5,
-          });
-          setOptions(mappedOptions.length > 0 ? mappedOptions : []);
-        } else {
-          setOptions([]);
-        }
-      } catch (error) {
-        console.error("Error fetching books:", error);
+      if (query) {
+        const result = await actionSearchBooks(query, 0, 5);
+        setOptions([]);
+        let mappedOptions = result.books.map((book, index) => ({
+          label: `${book.title} by ${book.authors}`,
+          imageUrl: book.smallThumbnail,
+          index,
+        }));
+        mappedOptions.push({
+          label: "See all results",
+          imageUrl: "",
+          index: 5,
+        });
+        mappedOptions = mappedOptions.length > 0 ? mappedOptions : []
+        setOptions(mappedOptions);
+      } else {
+        setOptions([]);
       }
-    },
+    } catch (error) {
+      console.error("Error fetching books:", error);
+    }
+  };
+
+  const debouncedSearchBooks = useCallback(
+    debounce(searchBooks, 300),
     []
   );
+
+  const handleInputChange = (_event: React.SyntheticEvent, query: string) => {
+    setInputValue(query);
+    debouncedSearchBooks(query);
+  }
 
   const handleOptionsRendering = useCallback(
     (props: HTMLAttributes<HTMLLIElement>, option: Option) => {
