bfba4d3e4cae6e78127b3e49ac5eb14e0de47f7b
Luis Antonio Castro <luisantoniokoyari@gmail.com>
2024-08-01 13:25:58 +0200

    changed prisma to db

--- db/users.ts
+++ db/users.ts
@@ -1,4 +1,4 @@
-import { prisma } from "@/db/db";
+import { db } from "@/db/db";
 
 export type User = {
   id: string;
@@ -7,25 +7,25 @@ export type User = {
 };
 
 export async function dbGetUsers() {
-  return await prisma.user.findMany({
+  return await db.user.findMany({
     orderBy: { id: "asc" },
   });
 }
 
 export async function dbGetUser(id: string) {
-  return await prisma.user.findUnique({
+  return await db.user.findUnique({
     where: { id },
   });
 }
 
 export async function dbGetUserByClerkId(clerkId: string) {
-  return await prisma.user.findUnique({
+  return await db.user.findUnique({
     where: { clerkId },
   });
 }
 
 export async function dbCreateUser(clerkId: string) {
-  return await prisma.user.create({
+  return await db.user.create({
     data: {
       clerkId,
     },
