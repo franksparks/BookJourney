ba8fa881b3ab24f206328a7b8a89182a636b78e2
Gloria Hornero <snootchie.boochies@gmail.com>
2024-08-04 14:26:43 +0200

    unknown author if there is any

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,7 +1,6 @@
 import { Book } from "@/actions/search-books";
 import { Table, TableBody, TableCell, TableRow } from "./ui/table";
 import { capitalizeFirstLetter } from "@/lib/capitalize";
-import { Button } from "./ui/button";
 import AddToList from './AddToList';
 
 type SearchResultProps = {
@@ -15,11 +14,12 @@ export default function SearchResult({ results }: SearchResultProps) {
             <TableBody>
                 {results.map((result, index) => (
                     <TableRow key={index}>
-                        <TableCell><img src={result.smallThumbnail || "../default_cover.jpg"} alt={`{result.title}cover`} style={{ width: '125px', height: '200px' }}/></TableCell>
-                        <TableCell><b className="text-base">{capitalizeFirstLetter(result.title)}</b><br />by {result.authors && (
-                            capitalizeFirstLetter(result.authors.join(" "))
-                        )}</TableCell>
-                        <TableCell><AddToList/></TableCell>
+                        <TableCell><img src={result.smallThumbnail || "../default_cover.jpg"} alt={`{result.title}cover`} style={{ width: '125px', height: '200px' }} /></TableCell>
+                        <TableCell><b className="text-base">{capitalizeFirstLetter(result.title)}</b><br />by {result.authors && result.authors.length > 0
+                            ? capitalizeFirstLetter(result.authors.join(" "))
+                            : "Unknown"}
+                        </TableCell>
+                        <TableCell><AddToList /></TableCell>
                     </TableRow>
                 ))}
             </TableBody>
