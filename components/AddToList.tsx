93cb8bc2d3e088a8e74c16e1b9bfea65a7fa8c75
Gloria Hornero <snootchie.boochies@gmail.com>
2024-08-04 14:21:57 +0200

    refactors add to list

--- components/AddToList.tsx
+++ components/AddToList.tsx
@@ -1,27 +1,28 @@
-
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuCheckboxItem } from "@radix-ui/react-dropdown-menu";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuCheckboxItem } from "@radix-ui/react-dropdown-menu";
 import { Button } from "./ui/button";
 
+const menuItems = [
+  { label: 'Read', value: 'read' },
+  { label: 'Currently reading', value: 'currently-reading' },
+  { label: 'Want to read', value: 'want-to-read' },
+];
+
 export default function AddToList() {
-    {
-        return (
-            <><Button className="rounded-r-none">Want to read</Button><DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                    <Button className="rounded-l-none">&#9660;</Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent>
-                    <DropdownMenuCheckboxItem
-                    >
-                        Read
-                    </DropdownMenuCheckboxItem>
-                    <DropdownMenuCheckboxItem>
-                        Currently reading
-                    </DropdownMenuCheckboxItem>
-                    <DropdownMenuCheckboxItem>
-                        Want to read
-                    </DropdownMenuCheckboxItem>
-                </DropdownMenuContent>
-            </DropdownMenu></>
-        )
-    }
+  return (
+    <>
+      <Button className="rounded-r-none">Want to read</Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button className="rounded-l-none">&#9660;</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {menuItems.map(item => (
+            <DropdownMenuCheckboxItem key={item.value}>
+              {item.label}
+            </DropdownMenuCheckboxItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
 }
