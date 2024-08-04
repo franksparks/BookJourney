dc8e1feb048dd6ac9fd335d1da6940cbe9d8e8bc
Gloria Hornero <snootchie.boochies@gmail.com>
2024-08-04 11:42:14 +0200

    removes unneed dependency

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
             setQuery(urlQuery);
             setAdvancedQuery(urlQuery);
         }
-    }, [window.location.search]);
+    }, []);
 
     useEffect(() => {
         handleSearch();
