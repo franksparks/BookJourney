4a289b5e16f1069afccb7e77d73e88e90dc650da
Gloria Hornero <snootchie.boochies@gmail.com>
2024-08-10 21:03:00 +0200

    fixes typo

--- actions/search-books-google.ts
+++ actions/search-books-google.ts
@@ -23,7 +23,7 @@ export async function actionSearchBooksGoogle(query: string, index: number, maxR
       categories: item.volumeInfo.categories,
       pages: item.volumeInfo.pages,
       publisher: item.volumeInfo.publiser,
-      publishDate: item.volumeInfo.publishedDate,
+      publishedDate: item.volumeInfo.publishedDate,
       language: item.volumeInfo.language,
       cover: item.volumeInfo.imageLinks?.smallThumbnail,
     };
