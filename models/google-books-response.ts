9b803a17c54e6c5bb059ee88376537276b1b3b70
Gloria Hornero <snootchie.boochies@gmail.com>
2024-08-10 21:04:04 +0200

    formatting

--- models/google-books-response.ts
+++ models/google-books-response.ts
@@ -1,31 +1,31 @@
 type Item = {
-    id: string;
-    volumeInfo: VolumeInfo;
-  };
-  
-  type ImageLinks = {
-    smallThumbnail: string;
-  };
+  id: string;
+  volumeInfo: VolumeInfo;
+};
 
-  type IndustriyIdentifier = {
-    type: string;
-    identifier: number;
-  };
-  
-  type VolumeInfo = {
-    title: string;
-    authors: string[];
-    imageLinks: ImageLinks;
-    description: string;
-    categories: string[];
-    pages: number;
-    industryIdentifiers: IndustriyIdentifier[];
-    publiser: string;
-    publishedDate: Date;
-    language: string;
-  };
-  
-  export type VolumesResponse = {
-    items: [Item];
-    totalItems: number;
-  };
+type ImageLinks = {
+  smallThumbnail: string;
+};
+
+type IndustriyIdentifier = {
+  type: string;
+  identifier: number;
+};
+
+type VolumeInfo = {
+  title: string;
+  authors: string[];
+  imageLinks: ImageLinks;
+  description: string;
+  categories: string[];
+  pages: number;
+  industryIdentifiers: IndustriyIdentifier[];
+  publiser: string;
+  publishedDate: Date;
+  language: string;
+};
+
+export type VolumesResponse = {
+  items: [Item];
+  totalItems: number;
+};
