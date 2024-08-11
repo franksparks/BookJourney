1f75abec09b6f03c657ebb864281f322669180a8
Gloria Hornero <snootchie.boochies@gmail.com>
2024-08-11 16:12:36 +0200

    formatting

--- app/context/books-search-context.js
+++ app/context/books-search-context.js
@@ -1,20 +1,31 @@
-"use client"
+"use client";
 
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useContext, useState } from "react";
 
 const BooksSearchContext = createContext();
 
 export const BooksSearchProvider = ({ children }) => {
-    const [results, setResults] = useState([]);
-    const [resetRadio, setResetRadio] = useState(true);
-    const [previewSearch, setPreviewSearch] = useState(false);
-    const [totalItems, setTotalItems] = useState(0);
+  const [results, setResults] = useState([]);
+  const [resetRadio, setResetRadio] = useState(true);
+  const [previewSearch, setPreviewSearch] = useState(false);
+  const [totalItems, setTotalItems] = useState(0);
 
-    return (
-        <BooksSearchContext.Provider value={{ results, setResults, resetRadio, setResetRadio, previewSearch, setPreviewSearch, totalItems, setTotalItems }}>
-            {children}
-        </BooksSearchContext.Provider>
-    );
+  return (
+    <BooksSearchContext.Provider
+      value={{
+        results,
+        setResults,
+        resetRadio,
+        setResetRadio,
+        previewSearch,
+        setPreviewSearch,
+        totalItems,
+        setTotalItems
+      }}
+    >
+      {children}
+    </BooksSearchContext.Provider>
+  );
 };
 
 export const useBooksSearchContext = () => useContext(BooksSearchContext);
