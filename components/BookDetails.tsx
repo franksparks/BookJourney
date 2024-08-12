fd8a6a5f394a1160337b2f2ad15982de1f5274fb
Gloria Hornero <snootchie.boochies@gmail.com>
2024-08-12 10:43:39 +0200

    adds genres

--- components/BookDetails.tsx
+++ components/BookDetails.tsx
@@ -1,5 +1,6 @@
 import { Book } from "@/models/book";
 import ReadMore from "./ReadMore";
+import { Separator } from "./ui/separator";
 
 type BookDetailsProps = {
     book: Book;
@@ -14,7 +15,13 @@ export default function BookDetails({ book }: BookDetailsProps) {
             <div className="flex w-screen justify-start flex-col">
                 <h1>{book.title}</h1>
                 {book.authors && book.authors.map((author, index) => (<h2 key={index}> {author} </h2>)) || (<h2> {'Unknown author'} </h2>)}
-                {book.description && <ReadMore text={book.description}/>}
+                {book.description && <><Separator className="my-4" /><ReadMore text={book.description} /> <Separator className="my-4" /></>}
+                {book.categories &&
+                    <><div className="mt-4 mb-4 font-bold">{'Genres'}</div><div className="flex h-5 items-center space-x-4">
+                        {book.categories.map((category, index) => (
+                            <><Separator orientation="vertical" /><div key={index}>{category}</div><Separator orientation="vertical" /></>
+                        ))}
+                    </div></>}
             </div>
         </div>)
 }
