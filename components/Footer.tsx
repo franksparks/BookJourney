6c5d112f998fac5e13398b61cc4044bb2bae89c9
Ferran Bals <ferranbals@gmail.com>
2024-08-13 15:14:37 +0200

    Added links to the team members.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,30 @@
 "use client";
 
-import { useDbUser } from "@/app/context/DbUserContext";
-import React from "react";
 import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function Footer() {
-  const { dbUser } = useDbUser();
   const { user } = useUser();
+  const year = new Date().getFullYear();
+  console.log(year);
 
   return (
-    <div className="bg-sky-600 h-50 text-white p-2">
-      <h1>Usuario Actual:</h1>
-      {user && <p>Hi, {user.firstName}</p>}
-      {dbUser && <p>{dbUser.id}</p>}
+    <div className="bg-sky-600 h-50 text-white p-2 text-center">
+      <p>&copy; {year} Copyright</p>
+      <div className="gap-5 flex flex-row justify-center">
+        <Link href="https://github.com/franksparks" target="_blank">
+          Ferran Bals
+        </Link>
+        <Link href="https://github.com/MadameSheema" target="_blank">
+          Gloria Hornero
+        </Link>
+        <Link href="https://github.com/Luisantonio88" target="_blank">
+          Luis Antonio Castro
+        </Link>
+        <Link href="https://github.com/vedderzeznick" target="_blank">
+          Martín Alarcón
+        </Link>
+      </div>
     </div>
   );
 }
