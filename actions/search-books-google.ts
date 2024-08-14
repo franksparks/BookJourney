a88e15a4aa20d819b120ff256be05426960b16dc
Gloria Hornero <snootchie.boochies@gmail.com>
2024-08-14 18:25:26 +0200

    fixes types

--- actions/search-books-google.ts
+++ actions/search-books-google.ts
@@ -19,8 +19,8 @@ export async function actionSearchBooksGoogle(query: string, index: number) {
     );
 
     return {
-      isbn10: isbn10Identifier ? isbn10Identifier.identifier : 0,
-      isbn13: isbn13Identifier ? isbn13Identifier.identifier : 0,
+      isbn10: isbn10Identifier ? isbn10Identifier.identifier : "",
+      isbn13: isbn13Identifier ? isbn13Identifier.identifier : "",
       authors: item.volumeInfo.authors,
       googleBooksId: item.id,
       title: item.volumeInfo.title,
