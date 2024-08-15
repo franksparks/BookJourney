4193883a8185a048fd56cc9fae32a8e704a6c18a
Ferran Bals <ferranbals@gmail.com>
2024-08-15 13:12:42 +0200

    ReadingStatusDropDown component working

--- components/ReadingStatusDropwdown.tsx
+++ components/ReadingStatusDropwdown.tsx
@@ -1,6 +1,7 @@
 import {
   actionGetBookStatusByBookIdAndUserId,
   actionInsertBookStatus,
+  actionUpdateBookStatus,
 } from "@/actions/book-status";
 import {
   actionGetBookByGoogleId,
@@ -12,17 +13,17 @@ import { BookStatus } from "@/models/book-status";
 import { ReadStatus } from "@prisma/client";
 import {
   DropdownMenu,
-  DropdownMenuCheckboxItem,
   DropdownMenuContent,
+  DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@radix-ui/react-dropdown-menu";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Button } from "./ui/button";
 
 const menuItems = [
-  { label: "Read", value: "read" },
-  { label: "Currently reading", value: "currently-reading" },
-  { label: "Want to read", value: "want-to-read" },
+  { label: "Read", value: ReadStatus.READ },
+  { label: "Currently reading", value: ReadStatus.READING },
+  { label: "Want to read", value: ReadStatus.WANT_TO_READ },
 ];
 
 type ReadingStatusDropwdownProps = {
@@ -46,13 +47,17 @@ export default function ReadingStatusDropwdown({
     const dbBook = await actionGetBookByGoogleId(book.googleBooksId);
 
     if (dbUser != null && dbBook != null) {
-      // Si el libro está en BBDD invoco el estado de lectura para este user
+      // Si el libro está en BBDD obtengo el estado de lectura
+      // de este libro para este usuario
       console.log("El libro ESTÁ en BBDD");
 
       const readingStatus: BookStatus =
         await actionGetBookStatusByBookIdAndUserId(dbBook.id!, dbUser.id);
 
-      console.log("READING STATUS: " + readingStatus.status);
+      if (readingStatus) {
+        console.log("READING STATUS: " + readingStatus.status);
+      }
+
       setStatus(readingStatus);
     } else {
       // Si el libro no está en BBDD, seteo status a null
@@ -61,35 +66,87 @@ export default function ReadingStatusDropwdown({
     }
   };
 
-  const handleDropdownClick = async () => {
+  const handleDropdownClick = async (status: ReadStatus) => {
     const res = await actionInsertBook(book);
-    const newStatus = await actionInsertBookStatus(
-      ReadStatus.WANT_TO_READ,
-      res,
-      dbUser
-    );
-    setStatus(newStatus);
+
+    if (!currentStatus) {
+      //Si el usuario no tiene entrada para bookStatus, hago insert
+      const newStatus = await actionInsertBookStatus(status, res, dbUser);
+      setStatus(newStatus);
+    } else {
+      //Si el usuario TIENE entrada para bookStatus, hago update
+      console.log("Update status here");
+
+      const updatedStatus = await actionUpdateBookStatus(
+        currentStatus.id,
+        status
+      );
+      setStatus(updatedStatus);
+    }
+
     getStatus();
+  };
+
+  const getSelectedLabel = () => {
+    if (currentStatus) {
+      const selectedItem = menuItems.find(
+        (item) => item.value === currentStatus.status
+      );
+      return selectedItem?.label || "Select status";
+    }
+    return "Want to read";
+  };
+
+  const getDropdownItems = () => {
+    if (currentStatus) {
+      return menuItems.filter(
+        (item) => item.value !== currentStatus.status
+      );
+    } else {
+      return menuItems.filter(
+        (item) => item.value !== ReadStatus.WANT_TO_READ
+      );
+    }
   };
 
   return (
     <>
-      <p>{currentStatus?.status}</p>
-      <Button onClick={handleDropdownClick} className="rounded-r-none">
-        Want to read
-      </Button>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button className="rounded-l-none">&#9660;</Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          {menuItems.map((item, index) => (
-            <DropdownMenuCheckboxItem key={index}>
-              {item.label}
-            </DropdownMenuCheckboxItem>
-          ))}
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <div>
+        <DropdownMenu>
+          <Button
+            onClick={() => {
+              handleDropdownClick(ReadStatus.WANT_TO_READ);
+            }}
+            className={
+              currentStatus
+                ? "rounded-l-none bg-blue-300 text-black"
+                : "rounded-l-none"
+            }
+          >
+            {getSelectedLabel()}
+          </Button>
+          <DropdownMenuTrigger asChild>
+            <Button className="rounded-l-none">&#9660;</Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="flex flex-col">
+            {getDropdownItems().map((item, index) => (
+              <DropdownMenuItem
+                key={index}
+                className={
+                  index === 0 && String(currentStatus) === item.value
+                    ? "rounded-l-none bg-blue-200"
+                    : "rounded-l-none"
+                }
+                onClick={() => {
+                  handleDropdownClick(item.value);
+                }}
+              >
+                <Button>{item.label}</Button>
+              </DropdownMenuItem>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
     </>
   );
 }
