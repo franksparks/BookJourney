fc7851165a3496ff8ad9276af7c648fefbb7c60c
Ferran Bals <ferranbals@gmail.com>
2024-08-15 13:14:33 +0200

    Fixed redirection on book details

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -14,31 +14,31 @@ export default function SearchResult({ books }: SearchResultProps) {
       <div className="grid grid-cols-2 grid-rows-5 gap-4 mt-5">
         {books.map((book: Book, index: any) => (
           <TableRow key={index}>
-            <BookNavigationWrapper id={book.googleBooksId} key={index}>
-              <div className="flex flex-row justify-between">
-                <TableCell>
+            <div className="flex flex-row justify-between">
+              <TableCell>
+                <BookNavigationWrapper id={book.googleBooksId} key={index}>
                   <img
                     src={book.smallCover || "../default_cover.jpg"}
                     alt={`{book.title}cover`}
                     style={{ width: "75", height: "150px" }}
                   />
-                </TableCell>
-                <TableCell className="flex flex-col w-96">
-                  <b className="text-base">{`${capitalizeFirstLetter(
-                    book.title
-                  )}`}</b>
-                  <div className="text-base">
-                    by{" "}
-                    {book.authors && book.authors.length > 0
-                      ? capitalizeFirstLetter(book.authors.join(" "))
-                      : "Unknown"}
-                  </div>
-                </TableCell>
-                <TableCell>
-                  <ReadingStatusDropwdown book={book} />
-                </TableCell>
-              </div>
-            </BookNavigationWrapper>
+                </BookNavigationWrapper>
+              </TableCell>
+              <TableCell className="flex flex-col w-96">
+                <b className="text-base">{`${capitalizeFirstLetter(
+                  book.title
+                )}`}</b>
+                <div className="text-base">
+                  by{" "}
+                  {book.authors && book.authors.length > 0
+                    ? capitalizeFirstLetter(book.authors.join(" "))
+                    : "Unknown"}
+                </div>
+              </TableCell>
+              <TableCell>
+                <ReadingStatusDropwdown book={book} />
+              </TableCell>
+            </div>
           </TableRow>
         ))}
       </div>
