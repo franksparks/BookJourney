440e6ba62c4c87566771a10f98d04ca01fb50e0a
Gloria Hornero <snootchie.boochies@gmail.com>
2024-08-25 11:48:49 +0200

    fixes deploy error

--- scripts/reviews/insert-review.ts
+++ scripts/reviews/insert-review.ts
@@ -10,25 +10,7 @@ if (process.argv.length != 5) {
 
 const [_bun, _script, user_id, book_id, review_comment] = process.argv;
 
-const rating_user: Prisma.UserCreateNestedOneWithoutListsInput = {
-  connect: {
-    id: user_id,
-  },
-};
-
-const rating_book: Prisma.BookCreateNestedOneWithoutListsInput = {
-  connect: {
-    id: book_id,
-  },
-};
-
-const new_review: Prisma.ReviewCreateInput = {
-  comment: review_comment,
-  user: rating_user,
-  book: rating_book,
-};
-
-const result = await actionInsertReview(new_review);
+const result = await actionInsertReview(review_comment, user_id, book_id);
 
 if (result != null) {
   console.log(result);
