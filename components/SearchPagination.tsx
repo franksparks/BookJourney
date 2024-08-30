9f28cf9de2c843727bd68f9f988ff1c446034dc9
Ferran Bals <ferranbals@gmail.com>
2024-08-30 18:36:58 +0200

    Removed vertical scroll and added styles to pagination

--- components/SearchPagination.tsx
+++ components/SearchPagination.tsx
@@ -1,18 +1,43 @@
 import Pagination from "@mui/material/Pagination";
 
 type SearchPaginationProps = {
-    setPage: (page: number) => void,
-    page: number,
-    totalItems: number,
-}
+  setPage: (page: number) => void;
+  page: number;
+  totalItems: number;
+};
 
-export default function SearchPagination({ setPage, page, totalItems }: SearchPaginationProps) {
-    const handleChange = (_event: React.ChangeEvent<unknown>, value: number) => {
-        setPage(value);
-    };
-    const numberOfPages = Math.ceil(totalItems / 10);
+export default function SearchPagination({
+  setPage,
+  page,
+  totalItems,
+}: SearchPaginationProps) {
+  const handleChange = (
+    _event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    setPage(value);
+  };
+  const numberOfPages = Math.ceil(totalItems / 10);
 
-    return (
-        <Pagination className="flex justify-center" count={numberOfPages} page={page} onChange={handleChange} />
-    )
+  return (
+    <Pagination
+      className="flex justify-center mb-1"
+      count={numberOfPages}
+      page={page}
+      onChange={handleChange}
+      variant="outlined"
+      sx={{
+        "& .MuiPaginationItem-root": {
+          color: "#ffffff", // Color del texto
+        },
+        "& .MuiPaginationItem-root.Mui-selected": {
+          backgroundColor: "#F97316", // Fondo del elemento seleccionado
+          color: "#ffffff", // Color del texto del elemento seleccionado
+        },
+        "& .MuiPaginationItem-root.Mui-disabled": {
+          color: "#e5e7eb", // Color para el elemento deshabilitado (opcional)
+        },
+      }}
+    />
+  );
 }
