fdf911e4a88782ad13a70312d223e3e731eb9450
Ferran Bals <ferranbals@gmail.com>
2024-09-01 18:35:48 +0200

    Removed some styles

--- components/BookDetails.tsx
+++ components/BookDetails.tsx
@@ -131,95 +131,93 @@ export default function BookDetails({ book }: BookDetailsProps) {
   }, [numericBookRating, bookInDb]);
 
   return (
-    <div className="bg-sky-50">
-      <div className="flex justify-center mt-10 ">
-        <div className="flex justify-center basis-1/4">
-          <div className="flex flex-col">
-            <img
-              className="mb-8"
-              src={book.cover || "../default_cover.jpg"}
+    <div className="flex justify-center mt-10 ">
+      <div className="flex justify-center basis-1/4">
+        <div className="flex flex-col">
+          <img
+            className="mb-8"
+            src={book.cover || "../default_cover.jpg"}
+          />
+          <div>
+            <ReadingStatusDropdown book={book} logged={logged} />
+          </div>
+          <div className="flex justify-center mt-7">
+            <ControlledRating
+              logged={logged}
+              bookRating={numericBookRating}
+              setBookRating={setNumericBookRating}
+              setFirstInteraction={setFirstInteraction}
             />
-            <div>
-              <ReadingStatusDropdown book={book} logged={logged} />
+          </div>
+          {!bookRating && (
+            <div className="flex justify-center mt-2">
+              {"Rate this book"}
             </div>
-            <div className="flex justify-center mt-7">
-              <ControlledRating
-                logged={logged}
-                bookRating={numericBookRating}
-                setBookRating={setNumericBookRating}
-                setFirstInteraction={setFirstInteraction}
+          )}
+          {bookRating && (
+            <div className="flex justify-center mt-2">
+              <ReviewDialogue
+                bookInDb={bookInDb!}
+                dbUser={dbUser}
+                numericBookRating={numericBookRating}
               />
             </div>
-            {!bookRating && (
-              <div className="flex justify-center mt-2">
-                {"Rate this book"}
-              </div>
-            )}
-            {bookRating && (
-              <div className="flex justify-center mt-2">
-                <ReviewDialogue
-                  bookInDb={bookInDb!}
-                  dbUser={dbUser}
-                  numericBookRating={numericBookRating}
-                />
-              </div>
-            )}
-          </div>
+          )}
         </div>
-        <div className="flex w-screen justify-start flex-col mr-4">
-          <div className="flex flex-row">
-            <h1 className="mr-4">{book.title}</h1>
-            {logged && <ReadRating value={averageBookRating} />}
-          </div>
-          {logged && (
-            <h2>{`Average: ${averageBookRating} - Number of ratings: ${numberOfRatings}`}</h2>
+      </div>
+      <div className="flex w-screen justify-start flex-col mr-4">
+        <div className="flex flex-row">
+          <h1 className="mr-4">{book.title}</h1>
+          {logged && <ReadRating value={averageBookRating} />}
+        </div>
+        {logged && (
+          <h2>{`Average: ${averageBookRating} - Number of ratings: ${numberOfRatings}`}</h2>
+        )}
+        <Separator className="my-4" />
+        {(book.authors &&
+          book.authors.map((author, index) => (
+            <h2 key={index}> {author} </h2>
+          ))) || <h2> {"Unknown author"} </h2>}
+        {book.description && <ReadMore text={book.description} />}
+        {book.categories && (
+          <>
+            <div className="mt-8 mb-4 font-bold">{"Genres"}</div>
+            <div className="flex h-5 items-center space-x-4">
+              {book.categories.map((category, index) => (
+                <>
+                  <Separator orientation="vertical" />
+                  <div key={index}>{category}</div>
+                  <Separator orientation="vertical" />
+                </>
+              ))}
+            </div>
+          </>
+        )}
+        {
+          <>
+            <div className="mt-8 font-bold">{"This edition"}</div>
+            <Separator className="my-4" />
+          </>
+        }
+        <div className="grid gap-4">
+          {book.pages !== 0 && (
+            <div className="flex items-center">
+              <div className="font-semibold">Pages</div>
+              <div className="ml-11">{book.pages}</div>
+            </div>
           )}
-          <Separator className="my-4" />
-          {(book.authors &&
-            book.authors.map((author, index) => (
-              <h2 key={index}> {author} </h2>
-            ))) || <h2> {"Unknown author"} </h2>}
-          {book.description && <ReadMore text={book.description} />}
-          {book.categories && (
-            <>
-              <div className="mt-8 mb-4 font-bold">{"Genres"}</div>
-              <div className="flex h-5 items-center space-x-4">
-                {book.categories.map((category, index) => (
-                  <>
-                    <Separator orientation="vertical" />
-                    <div key={index}>{category}</div>
-                    <Separator orientation="vertical" />
-                  </>
-                ))}
-              </div>
-            </>
+          {book.language && (
+            <div className="flex items-center">
+              <div className="font-semibold">Published</div>
+              <div className="ml-4">{book.publishedDate}</div>
+            </div>
           )}
-          {
-            <>
-              <div className="mt-8 font-bold">{"This edition"}</div>
-              <Separator className="my-4" />
-            </>
-          }
-          <div className="grid gap-4">
-            {book.pages !== 0 && (
-              <div className="flex items-center">
-                <div className="font-semibold">Pages</div>
-                <div className="ml-11">{book.pages}</div>
-              </div>
-            )}
-            {book.language && (
-              <div className="flex items-center">
-                <div className="font-semibold">Published</div>
-                <div className="ml-4">{book.publishedDate}</div>
-              </div>
-            )}
-            {book.language && (
-              <div className="flex items-center">
-                <div className="font-semibold">Language</div>
-                <div className="ml-4">{book.language.toUpperCase()}</div>
-              </div>
-            )}
-          </div>
+          {book.language && (
+            <div className="flex items-center">
+              <div className="font-semibold">Language</div>
+              <div className="ml-4">{book.language.toUpperCase()}</div>
+            </div>
+          )}
         </div>
       </div>
     </div>
