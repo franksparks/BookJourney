12804b9e9c7192543847bb776bd51269ad11fa00
Gloria Hornero <snootchie.boochies@gmail.com>
2024-09-11 21:14:32 +0200

    adds default value if the title is not present

--- components/BookCardAdvanced.tsx
+++ components/BookCardAdvanced.tsx
@@ -41,13 +41,11 @@ export default function BookCardAdvanced({ book }: bookCardAdvancedProps) {
       </div>
       <div className="flex flex-col justify-center w-2/3 gap-2 m-2 mr-5 cursor-default">
         <BookNavigationWrapper id={book.googleBooksId}>
-          {book.title && (
-            <Tooltip arrow title={book.title} placement="top-start">
-              <b className="text-base line-clamp-1 text-sky-700">{`${capitalizeFirstLetter(
-                book.title
-              )}`}</b>
-            </Tooltip>
-          )}
+          <Tooltip arrow title={book.title} placement="top-start">
+            <b className="text-base line-clamp-1 text-sky-700">{`${capitalizeFirstLetter(
+              book.title || "Title not available"
+            )}`}</b>
+          </Tooltip>
         </BookNavigationWrapper>
         <Tooltip arrow title={book.authors} placement="bottom-start">
           <div className="text-base line-clamp-1 text-sky-700">
