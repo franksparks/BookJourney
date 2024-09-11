64ee5cf8dbdc1e5daf05f30a4142f447589de566
Gloria Hornero <snootchie.boochies@gmail.com>
2024-09-11 11:19:43 +0200

    book details bug fix

--- components/Reviews.tsx
+++ components/Reviews.tsx
@@ -72,6 +72,7 @@ export default function Reviews({
             { userReview: [], otherMembersReviews: [] }
           );
 
+        setBookReviews(userReview);
         setBookReviews(otherMembersReviews);
       }
     }
