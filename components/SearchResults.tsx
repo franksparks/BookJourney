371216fde31aaeb7c692ab277d6ef7e15d9693a5
Martin Alarcon <cristianma.2109@gmail.com>
2024-09-14 21:55:15 +0200

    solve conflicts

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -10,7 +10,12 @@ export default function SearchResult({ books }: SearchResultProps) {
     <div className="grid grid-cols-2 grid-rows-5 gap-2 m-2 w-4/5 h-full">
       {books.map((book: DbBook, index) => (
         <div key={index}>
-          <BookCardAdvanced book={book} deleteVisible={false} onDelete={() => {}} onStatusChange={() => {}} />
+          <BookCardAdvanced
+            book={book}
+            deleteVisible={false}
+            onDelete={() => {}}
+            onStatusChange={() => {}}
+          />
         </div>
       ))}
     </div>
