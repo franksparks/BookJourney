f3baf20c1b921d4f21a3371300f187f979a8ab73
Ferran Bals <ferranbals@gmail.com>
2024-09-14 14:42:32 +0200

    Improving lists links

--- components/lists/LandingListsCard.tsx
+++ components/lists/LandingListsCard.tsx
@@ -9,45 +9,41 @@ export default function ListsCard() {
   const router = useRouter();
 
   return (
-    <div className="flex flex-col justify-start rounded-xl shadow-lg shadow-orange-700 bg-orange-500 pl-8 pr-8 pt-4 text-orange-50 h-1/3">
+    <div className="flex flex-col justify-start rounded-xl shadow-lg shadow-orange-700 bg-orange-500 p-8 text-orange-50 h-1/3">
       <h1 className="font-light text-orange-50 text-center border-b-2">
         My Lists
       </h1>
       {!dbUser && (
-        <div className="flex flex-col justify-center items-center w-full">
+        <div className="flex flex-col justify-center items-center h-full w-full">
           <p className="mb-2">Login to create and view lists!</p>
         </div>
       )}
       {dbUser && (
-        <div className="flex flex-col w-full justify-evenly">
-          <div className="flex flex-row justify-around">
-            <Button
-              className="text-lg p-6"
-              onClick={() => router.push("/lists?listId=READ")}
-            >
-              Read books
-            </Button>
-            <Button
-              className="text-lg p-6"
-              onClick={() => router.push("/lists?listId=READING")}
-            >
-              Books you are reading
-            </Button>
-          </div>
-          <div className="flex flex-row justify-around">
-            <Button
-              className="text-lg p-6"
-              onClick={() => router.push("/lists?listId=WANT_TO_READ")}
-            >
-              Books you want to read
-            </Button>
-            <Button
-              className="text-lg p-6"
-              onClick={() => router.push("/lists")}
-            >
-              All lists
-            </Button>
-          </div>
+        <div className="grid grid-cols-2 gap-4 w-full h-full place-content-evenly">
+          <Button
+            className="text-lg p-6 w-full"
+            onClick={() => router.push("/lists?listId=READ")}
+          >
+            Read books
+          </Button>
+          <Button
+            className="text-lg p-6 w-full"
+            onClick={() => router.push("/lists?listId=READING")}
+          >
+            Books you are reading
+          </Button>
+          <Button
+            className="text-lg p-6 w-full"
+            onClick={() => router.push("/lists?listId=WANT_TO_READ")}
+          >
+            Books you want to read
+          </Button>
+          <Button
+            className="text-lg p-6 w-full"
+            onClick={() => router.push("/lists")}
+          >
+            All lists
+          </Button>
         </div>
       )}
     </div>
