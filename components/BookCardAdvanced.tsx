d0d669d83ddb3d116de7c39084155cef0c175b46
Ferran Bals <ferranbals@gmail.com>
2024-09-15 19:42:08 +0200

    test on dynamic sizes

--- components/BookCardAdvanced.tsx
+++ components/BookCardAdvanced.tsx
@@ -32,7 +32,7 @@ export default function BookCardAdvanced({
   useEffect(() => {
     const handleResize = () => {
       const height = window.innerHeight;
-      if (height > 1000) {
+      if (height > 600) {
         setImageSize("large");
       } else {
         setImageSize("small");
@@ -75,21 +75,42 @@ export default function BookCardAdvanced({
       </div>
 
       <div className="flex flex-col justify-center w-2/3 m-2 mr-5 cursor-default">
-        <BookNavigationWrapper id={book.googleBooksId}>
-          <Tooltip arrow title={book.title} placement="top-start">
-            <b className="text-base line-clamp-1 text-sky-700">{`${capitalizeFirstLetter(
-              book.title || "Title not available"
-            )}`}</b>
-          </Tooltip>
-        </BookNavigationWrapper>
-        <Tooltip arrow title={book.authors} placement="bottom-start">
-          <div className="text-base line-clamp-1 text-sky-700">
-            by{" "}
-            {book.authors && book.authors.length > 0
-              ? capitalizeFirstLetter(book.authors.join(" "))
-              : "Unknown"}
-          </div>
-        </Tooltip>
+        {imageSize === "large" ? (
+          <>
+            <BookNavigationWrapper id={book.googleBooksId}>
+              <Tooltip arrow title={book.title} placement="top-start">
+                <b className="text-base line-clamp-1 text-sky-700">{`${capitalizeFirstLetter(
+                  book.title || "Title not available"
+                )}`}</b>
+              </Tooltip>
+            </BookNavigationWrapper>
+            <Tooltip arrow title={book.authors} placement="bottom-start">
+              <div className="text-base line-clamp-1 text-sky-700">
+                by{" "}
+                {book.authors && book.authors.length > 0
+                  ? capitalizeFirstLetter(book.authors.join(" "))
+                  : "Unknown"}
+              </div>
+            </Tooltip>
+          </>
+        ) : (
+          <>
+            <BookNavigationWrapper id={book.googleBooksId}>
+              <Tooltip arrow title={book.title} placement="top-start">
+                <b className="text-base line-clamp-1 text-sky-700">
+                  {`${capitalizeFirstLetter(
+                    book.title || "Title not available"
+                  )}`}{" "}
+                  by{" "}
+                  {book.authors && book.authors.length > 0
+                    ? capitalizeFirstLetter(book.authors.join(" "))
+                    : "Unknown"}
+                </b>
+              </Tooltip>
+            </BookNavigationWrapper>
+          </>
+        )}
+
         <div className="flex z-50">
           <ReadingStatusDropdown
             book={book}
